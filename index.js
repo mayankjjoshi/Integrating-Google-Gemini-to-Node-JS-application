aa4370865ceaf3525bd74a4ccc56a2ca01027c63
mayank <mayankjoshi82003@gmail.com>
2024-09-06 20:35:56 +0530

    add cors

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import express from "express";
 import dotenv from 'dotenv';
 import path from "path"
 import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 
 
-
+pp.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE']}));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
