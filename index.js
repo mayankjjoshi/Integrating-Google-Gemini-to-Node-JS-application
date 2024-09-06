8457e113eab548ec388a478d821c07b5dfe97a51
mayank <mayankjoshi82003@gmail.com>
2024-09-06 20:39:51 +0530

    update cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 
-pp.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE']}));
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE']}));
 app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
